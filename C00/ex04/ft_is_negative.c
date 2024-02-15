4d2bcfc455b1cabfefa3d8d57476f4729dfd976e
kaietanpaul <kaietanpaul@gmail.com>
2024-02-15 12:27:06 +0100

    check if positive or negative number

---
+++ C00/ex04/ft_is_negative.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+
+void	ft_is_negative(int n);
+
+void	ft_is_negative(int n) {
+
+	int	i = 0;
+
+	if (n >= i)
+		write(1, "P", 1);
+	else
+		write(1, "N", 1);
+}
+
+int	main(void) {
+
+	int	a = 0;
+	ft_is_negative(a);
+	return(0);
+}
